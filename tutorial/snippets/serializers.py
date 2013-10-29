9a2e6e88cd80a8ac2b49bc7a7ee2b63e9eaee36f
unknown <lil_bill12000@yahoo.com>
2013-10-29 00:37:30 -0400

    first tutorial django REST framework complete

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,42 @@
+from django.forms import widgets
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES,STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.Field()  #Note: 'Field' is an untyped read-only field.
+    title = serializers.CharField(required=False,
+                                  max_length=100)
+    code = serializers.CharField(widget=widgets.Textarea,
+                                 max_length=100000)
+    linenos= serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
+                                   default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES,
+                                    default='friendly')
+
+
+
+    def restore_object(self, attrs, instance=None):
+        """
+        Create or update a new snippet instance, given a dictionary
+        of deserialized field values.STYLE_CHOICES
+        Note that if we don't define this method, then deserializing
+        data will simple return a dictionary of items.
+        """
+        if instance:
+            # Update existing instance
+            instance.title = attrs.get('title', instance.title)
+            instance.code = attrs.get('code', instance.code)
+            instance.linenos = attrs.get('linenos', instance.linenos)
+            instance.language = attrs.get('language',instance.language)
+            instance.style= attrs.get('style', instance.style)
+            return instance
+
+        # Create a new instance
+        return Snippet(**attrs)
+
+
+class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
