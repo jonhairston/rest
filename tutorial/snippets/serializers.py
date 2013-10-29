934fbd3ae1a7011a43edf72578de2063c72b46be
unknown <lil_bill12000@yahoo.com>
2013-10-29 02:45:13 -0400

    tut3 using generic classed based views...gives you a whole bunch of functionality with very few lines of code...

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,14 +4,14 @@ from snippets.models import Snippet, LANGUAGE_CHOICES,STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.Serializer):
-    pk = serializers.Field()  #Note: 'Field' is an untyped read-only field.
+    pk = serializers.Field()  # Note: 'Field' is an untyped read-only field.
     title = serializers.CharField(required=False,
                                   max_length=100)
     code = serializers.CharField(widget=widgets.Textarea,
                                  max_length=100000)
     linenos= serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
-                                   default='python')
+                                 default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES,
                                     default='friendly')
 
